ed366bcbbf34cdbf7ba70e7ee7d2869bd9b594f4
Prabhav Rajeev <prabhavrajeev.51@gmail.com>
2020-02-03 16:12:07 +0530

    ui

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,14 +4,11 @@ import "firebase/database";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyByyX1eUf6zDjDBCJA0DN-7K2xDtWq43R4",
-  authDomain: "dyuthi-bd052.firebaseapp.com",
-  databaseURL: "https://dyuthi-bd052.firebaseio.com",
-  projectId: "dyuthi-bd052",
-  storageBucket: "dyuthi-bd052.appspot.com",
-  messagingSenderId: "173809148500",
-  appId: "1:173809148500:web:1321954016c2f2cc32c3c1",
-  measurementId: "G-8TYM87Y5V3"
+  /*
+  
+    FIREBASE CONFIG
+ 
+ */
 };
 
 const secondaryApp = firebase.initializeApp(firebaseConfig, "Secondary");
