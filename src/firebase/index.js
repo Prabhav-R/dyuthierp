7c40f579e7b0363fac2838bc20b0bd0601894779
Prabhav Rajeev <prabhavrajeev.51@gmail.com>
2020-02-03 12:03:08 +0530

    hide firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,26 +4,12 @@ import "firebase/database";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyByyX1eUf6zDjDBCJA0DN-7K2xDtWq43R4",
-  authDomain: "dyuthi-bd052.firebaseapp.com",
-  databaseURL: "https://dyuthi-bd052.firebaseio.com",
-  projectId: "dyuthi-bd052",
-  storageBucket: "dyuthi-bd052.appspot.com",
-  messagingSenderId: "173809148500",
-  appId: "1:173809148500:web:1321954016c2f2cc32c3c1",
-  measurementId: "G-8TYM87Y5V3"
+  /*
+  
+  FIREBASE CONFIG HERE
+  
+ */
 };
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBDIZaaWCpYrjJFdy-IXGzKgHD9C8Nh_ZI",
-//   authDomain: "dyuthierp.firebaseapp.com",
-//   databaseURL: "https://dyuthierp.firebaseio.com",
-//   projectId: "dyuthierp",
-//   storageBucket: "dyuthierp.appspot.com",
-//   messagingSenderId: "799799573144",
-//   appId: "1:799799573144:web:f1ecafc6ead307a902eeed",
-//   measurementId: "G-SPRV8QNX7F"
-// };
 
 const secondaryApp = firebase.initializeApp(firebaseConfig, "Secondary");
 // Initialize Firebase
